6de0b6ec587bcaa9d9d736113ddbfe26a6ec5286
Chinmay Garde <chinmaygarde@gmail.com>
2011-01-06 09:35:30 -0500

    Instapaper integration done

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -37,3 +37,37 @@ function getInterestingness(user_id) {
 		$("#results").show("slow");
 	});
 }
+
+function getLongUrl (url, link_id) {
+	// Get long URL
+	$("#long_url_link_" + link_id).html("");
+	$.getJSON('http://api.longurl.org/v2/expand?url=' + url + '&format=json&callback=?', function(data) {
+		$("#short_url_" + link_id).html(data["long-url"]);
+	});
+}
+
+function submitFormToInstapaper() {
+	var username = $("#instapaper_username").val();
+	var password = $("#instapaper_password").val();
+	var url = escape($("#instapaper_url").val());
+	var title = $("#instapaper_title").val();
+	
+	$.getJSON('https://www.instapaper.com/api/add?username=' + username + '&password=' + password + '&url=' + url + '&title=' + title + '&jsonp=?', function(data) {
+		if (data.status == 201 || data.status == 200) {
+			$("#instapaper_dialog").dialog('close');
+			$("#instapaper_title").val("");
+			$("#instapaper_url").val("");
+			$("#instapaper_error").html("");
+		}
+		else {
+			$("#instapaper_error").html("Failed with response " + data.status);
+		};
+	});
+	
+	
+}
+
+function showInstapaperForm(url) {
+	$("#instapaper_url").val(url);
+	$("#instapaper_dialog").dialog('open');
+}
